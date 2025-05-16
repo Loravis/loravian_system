503021d7b6a3e2e90c0eb2f1c0731781819f1a25
Loravis <loravis@proton.me>
2025-05-16 09:31:46 +0200

    Added basic bot functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
-int main(void) {
+#include <dpp/dpp.h>
+#include "../env/env.h"
+
+int main() {
+  dpp::cluster bot(BOT_TOKEN);
+  bot.on_log(dpp::utility::cout_logger());
+  bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+    if (event.command.get_command_name() == "test") {
+      event.reply("Hello world!");
+    }
+  });
+
+  bot.on_ready([&bot](const dpp::ready_t& event) {
+    if (dpp::run_once<struct register_bot_commands>()) {
+      bot.global_command_create(dpp::slashcommand("test", "Test the bot.", bot.me.id));
+    }
+  });
   
+  bot.start(dpp::st_wait);
 }
